2c3e40e5587e4664d0660b88ef096b58c8125080
Mikael Augusto <mikaelaugustodev@gmail.com>
2023-01-23 13:55:38 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("agradecimento").style.display="none";
+
+
+document.getElementById("um").addEventListener('click',function a(){
+	document.getElementById("selecionou").innerHTML=1});
+
+
+
+document.getElementById("dois").addEventListener('click',function a(){
+	document.getElementById("selecionou").innerHTML=2});
+
+
+document.getElementById("tres").addEventListener('click',function a(){
+	document.getElementById("selecionou").innerHTML=3});
+
+
+document.getElementById("quatro").addEventListener('click',function a(){
+	document.getElementById("selecionou").innerHTML=4});
+
+
+document.getElementById("five").addEventListener('click',function a(){
+	document.getElementById("selecionou").innerHTML=5});
+	
+	
+document.getElementById("enviar").addEventListener('click',function a(){
+document.getElementById("container").style.display="none";
+document.getElementById("agradecimento").style.display="block"});
